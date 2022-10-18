76b9e0f7593707d01a8b56c4a31ede69a8cbb25b
MartinDeMundia <mugambi.m.mundia@gmail.com>
2022-10-18 10:12:09 +0300

    fix : edit of the test
    -edited pytest function test_health, to check pytest

--- test_main.py
+++ test_main.py
@@ -25,7 +25,7 @@ def client():
 def test_health(client):
     response = client.get('/')
     assert response.status_code == 200
-    assert response.json == 'Healthy'
+    assert response.json == 'My Final Project Homepage!!'
 
 
 def test_auth(client):

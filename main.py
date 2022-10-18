5e17e2f3f6414c81e25a01ca16bea73c4718f04b
MartinDeMundia <mugambi.m.mundia@gmail.com>
2022-10-18 10:13:51 +0300

    fix : echo statement
    -function change on def health()

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ def require_jwt(function):
 
 @APP.route('/', methods=['POST', 'GET'])
 def health():
-    return jsonify("Healthy App")
+    return jsonify("My Final Project Homepage!!")
 
 
 @APP.route('/auth', methods=['POST'])
